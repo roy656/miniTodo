ab9f4f5b9fa37b3dad8f00c42db917b7fc1c2dcd
Roy C <v0o0605@gmail.com>
2022-12-09 11:20:44 +0900

    간단한 로그 추가

--- src/main/java/com/personal/minitodo/controller/TodoController.java
+++ src/main/java/com/personal/minitodo/controller/TodoController.java
@@ -4,6 +4,7 @@ import com.personal.minitodo.model.TodoEntity;
 import com.personal.minitodo.model.TodoRequest;
 import com.personal.minitodo.model.TodoResponse;
 import com.personal.minitodo.service.TodoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping("/todo")
+@Slf4j
 @RestController
 public class TodoController {
 
@@ -26,7 +28,7 @@ public class TodoController {
     // 파라미터에 필요한 어노테이션 잊지말기
     @PostMapping
     public ResponseEntity<TodoResponse> creatTodo(@RequestBody TodoRequest todoRequest) {
-
+        log.info("Create");
         if (ObjectUtils.isEmpty(todoRequest.getTitle()))
             return ResponseEntity.badRequest().build();
 
@@ -44,30 +46,34 @@ public class TodoController {
 
     @GetMapping
     public ResponseEntity<List<TodoResponse>> readAllTodo() {
-
+        log.info("Read All");
         return ResponseEntity.ok(todoService.readAll());
     }
 
     @GetMapping("{id}")
     public ResponseEntity<TodoResponse> readOneTodo(@PathVariable Long id) {
+        log.info("Read One");
         TodoEntity result = todoService.readOneTodo(id);
         return ResponseEntity.ok(new TodoResponse(result));
     }
 
     @PatchMapping("{id}")
     public ResponseEntity<TodoResponse> updateTodo(@RequestBody TodoRequest todoRequest, @PathVariable Long id) {
+        log.info("Update");
         TodoEntity result = todoService.update(todoRequest, id);
         return ResponseEntity.ok(new TodoResponse(result));
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Object> deleteOneTodo(@PathVariable Long id) {
+        log.info("Delete One");
         todoService.deleteOneTodo(id);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping
     public ResponseEntity<Object> deleteAllTodo() {
+        log.info("Delete All");
         todoService.deleteAllTodo();
         return ResponseEntity.ok().build();
     }
